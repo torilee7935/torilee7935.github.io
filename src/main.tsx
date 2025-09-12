c3632b1e6ed7c957f80a5d611187097f98f8ba93
Tori Lee <torilee@Toris-Air.attlocal.net>
2025-09-12 09:53:14 -0400

    Music player removal

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,6 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <IndexPage />
-    <MusicPlayer />
+    {/* <MusicPlayer /> */}
   </React.StrictMode>
 )
